10f6d0f4ad5f063b140394f5fece7b8f1bd68d6b
GrinevichEvgen <grinevichevgen@gmail.com>
2024-09-12 13:14:32 +0300

    add account

---
+++ account/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'registration/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+        return render(request, 'registration/register.html', {'form': form})
+
+
+class ProfileView(View):
+    def get(self, request):
+        return render(request, 'profile.html')
