1b11307c005a406869463aa0b9d7f66ff9ec48f5
GrinevichEvgen <grinevichevgen@gmail.com>
2024-09-12 19:34:07 +0300

    add account

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from .views import RegisterView, ProfileView
 
 urlpatterns = [
+
     path('login/', auth_views.LoginView.as_view(template_name='notes/login.html'), name='login'),  # URL для входа
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
