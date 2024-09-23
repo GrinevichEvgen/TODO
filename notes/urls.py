51ad3ff827b30aab84ccc8661f0d984c3cba2873
GrinevichEvgen <grinevichevgen@gmail.com>
2024-09-23 23:40:03 +0300

    add account

--- notes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register, home, login_view
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('register/', register, name='register'),
-    path('login/', login_view, name='login'),
-]
